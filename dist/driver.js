54de4e4ba09b2b757ca98e552ac24c8f0d353042
abi-noel <abi_noel7@yahoo.com>
2025-02-22 17:39:02 -0500

    Make the circle follow the cursor at a rate proportional to its distance Ticket #4

--- dist/driver.js
+++ dist/driver.js
@@ -6,7 +6,8 @@ import { ctx, canvas } from "./canvas.js";
 var Driver = /** @class */ (function () {
     function Driver() {
         var _this = this;
-        this.circle = new Circle(canvas.width / 2, canvas.height / 2, 1, Math.PI * 2, 20);
+        this.circle = new Circle(Math.random() * (canvas.width - 20 * 2) + 20, Math.random() * (canvas.height - 20 * 2) + 20, 1, Math.PI * 2, 20);
+        this.pounceThreshhold = 200;
         // Store mouse's position
         this.mousePosition = { x: 0, y: 0 };
         /**
@@ -17,9 +18,16 @@ var Driver = /** @class */ (function () {
             requestAnimationFrame(_this.animate);
             // Clear canvas
             ctx.clearRect(0, 0, innerWidth, innerHeight);
-            // Update circle position to mouse position
-            _this.circle.position.x = _this.mousePosition.x;
-            _this.circle.position.y = _this.mousePosition.y;
+            // Get the distance between the mouse and the circle
+            var distanceX = _this.mousePosition.x - _this.circle.position.x;
+            var distanceY = _this.mousePosition.y - _this.circle.position.y;
+            var distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+            // If the distance is greater than the pounce threshhold
+            if (distance > _this.pounceThreshhold) {
+                // Scale down the distance vector and add that to the circle's position
+                _this.circle.position.x += distanceX * 0.012;
+                _this.circle.position.y += distanceY * 0.012;
+            }
             // Redraw circle
             _this.circle.draw();
         };
--- driver.ts
+++ driver.ts
@@ -15,12 +15,14 @@ export interface Vector {
  */
 class Driver {
   public circle = new Circle(
-    canvas!.width / 2,
-    canvas!.height / 2,
+    Math.random() * (canvas!.width - 20 * 2) + 20,
+    Math.random() * (canvas!.height - 20 * 2) + 20,
     1,
     Math.PI * 2,
     20
   );
+
+  public pounceThreshhold = 200;
 
   // Store mouse's position
   public mousePosition: Vector = { x: 0, y: 0 };
@@ -44,9 +46,17 @@ class Driver {
     // Clear canvas
     ctx!.clearRect(0, 0, innerWidth, innerHeight);
 
-    // Update circle position to mouse position
-    this.circle.position.x = this.mousePosition.x;
-    this.circle.position.y = this.mousePosition.y;
+    // Get the distance between the mouse and the circle
+    const distanceX = this.mousePosition.x - this.circle.position.x;
+    const distanceY = this.mousePosition.y - this.circle.position.y;
+    const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
+
+    // If the distance is greater than the pounce threshhold
+    if (distance > this.pounceThreshhold) {
+      // Scale down the distance vector and add that to the circle's position
+      this.circle.position.x += distanceX * 0.012;
+      this.circle.position.y += distanceY * 0.012;
+    }
 
     // Redraw circle
     this.circle.draw();
